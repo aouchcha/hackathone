4e51770799c0669a2d2fb7b41b3ca35bd0823032
Your Name <you@example.com>
2025-02-15 22:46:50 +0100

    lv

---
+++ BackEnd/db/ConnectTodb.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+	"forum/BackEnd/config"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func ConnectTodb(name string) error {
+	// open and create the db if the db is not exist
+	var err error
+	config.Config.Database, err = sql.Open("sqlite3", name)
+	if err != nil {
+		return err
+	}
+	// ping the db to see if it connected
+	if err = config.Config.Database.Ping(); err != nil {
+		return err
+	}
+	Schema, err := os.ReadFile("server/db/schema/setup.sql")
+	if err != nil {
+		return err
+	}
+	_, err = config.Config.Database.Exec(string(Schema))
+	if err != nil {
+
+		return err
+	}
+	return nil
+}
